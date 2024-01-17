055ce3452fce5c9d8bf125f4542993ab9fa0a22a
loukhai <loukhairabia@gmail.com>
2024-01-17 01:27:32 +0100

    interface

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
  *  */
 // basic interface
 // is simply a named set of properties that describe a shape of an object
+// By convention, the interface names are in the camel case.
+
 interface Person {
   name: string;
   age: number;
@@ -10,7 +12,7 @@ interface Person {
 }
 
 const person1: Person = {
-  name: "rabia",
+  name: 'rabia',
   age: 28,
 };
 
@@ -19,7 +21,7 @@ const person1: Person = {
  * */
 // that used to describe the type of arguments and the return values for a function
 interface Greeting {
-  // (name: string, age: number): string;
+  // (name: string, age: number): string;  //instead of do that
   (p: Person): string;
 }
 // const greetingPerson2: Greeting = (n, a) => {
@@ -28,7 +30,7 @@ interface Greeting {
 // const grb2 = greetingPerson2("y", 33);
 // console.log(grb2);
 const greetingPerson: Greeting = (p) => {
-  return `hello ${p.name} you have ${p.age}  `;
+  return `hello ${p.name} you have ${p.age}`;
 };
 const grb = greetingPerson(person1);
 console.log(grb);
